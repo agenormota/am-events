f76483ba7f6f6947e8ad291d4e8ea2ebb8d6083f
agenormota <agenor.mota@gmail.com>
2022-02-08 18:59:01 -0500

    Update event class

--- Back/src/AM.Events.API/AM.Events.API/Controllers/EventController.cs
+++ Back/src/AM.Events.API/AM.Events.API/Controllers/EventController.cs
@@ -1,3 +1,4 @@
+using AM.Events.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AM.Events.API.Controllers
@@ -6,8 +7,11 @@ namespace AM.Events.API.Controllers
     [Route("[controller]")]
     public class EventController : ControllerBase
     {
-       
 
+        private static readonly string[] Summaries = new[]
+                {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
         private readonly ILogger<EventController> _logger;
 
         public EventController(ILogger<EventController> logger)
@@ -15,6 +19,17 @@ namespace AM.Events.API.Controllers
             _logger = logger;
         }
 
-        
+        [HttpGet(Name = "GeEvents")]
+        public IEnumerable<Event> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Event
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
     }
 }
